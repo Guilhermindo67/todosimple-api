969dfc0750d68d11d3a70e34d3b977b88263c45f
Guilhermindo67 <guilhermindo67@gmail.com>
2025-01-03 23:47:16 -0300

    Create Task Model

--- src/main/java/com/guilhermealmeida/todosimple/models/User.java
+++ src/main/java/com/guilhermealmeida/todosimple/models/User.java
@@ -1,5 +1,7 @@
 package com.guilhermealmeida.todosimple.models;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
@@ -10,6 +12,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
@@ -46,20 +49,31 @@ public class User {
     @Size(groups = {CreateUser.class, UpdateUser.class}, min = 8, max = 60)
     private String password;
     
-    //private list<Task> tasks = new ArrayList<task>();
+    @OneToMany(mappedBy = "user")
+    private List<Task> tasks = new ArrayList<Task>();
 
-    //Constructor
-
+    //Constructors
+    
     public User(){
-
+        
     }
-
+    
     public User(Long id, String name, String password){
         this.id = id;
         this.name = name;
         this.password = password;
     }
 
+    //METODS
+    
+    public List<Task> getTasks() {
+        return tasks;
+    }
+
+    public void setTasks(List<Task> tasks) {
+        this.tasks = tasks;
+    }
+        
     public String getPassword() {
         return password;
     }
